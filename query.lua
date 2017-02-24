5ac36c1bf80ae911b495047c7c416e34dd650092
MinePlayersPE <yudhit.angga@gmail.com>
2017-02-24 18:05:44 +0700

    Update query.lua

--- query.lua
+++ query.lua
@@ -5,8 +5,10 @@ local module = {}
 function module:ConnectToServer(url)
 	if type(url) == "string" then
 		if string.sub(url, #url, #url) == "/" then
+			noslash = true
 			succ = pcall(game.HttpService:GetAsync(url .. "conntest.php"))
 		else
+			noslash = false
 			succ = pcall(game.HttpService:GetAsync(url .. "/conntest.php"))
 		end
 	end
